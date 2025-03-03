9ff2ec5dd708c234072dd7fce1984e8d357f1b7b
Adedayo Aderibigbe <adedayoayzee@gmail.com>
2025-03-03 12:47:55 +0100

    Ignore .env and node_modules, push to b-s-sender

--- src/sendSMS.ts
+++ src/sendSMS.ts
@@ -5,7 +5,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const apiKey = process.env.TERMII_API_KEY;
-const url = 'https://api.ng.termii.com/api/sms/send';
+const url = 'https://api.ng.termii.com/api/sms/send/bulk';
 
 
 interface SendSMSPayload {
@@ -29,7 +29,7 @@ const sendSMS = async ({ to, message }: SendSMSPayload): Promise<boolean> => {
         console.log("Response:", response.data);
         return true;  // ✅ Return true if successful
     } catch (error) {
-        /// useing type assertion to get the error response
+        /// using type assertion to get the error response here.
         const err = error as any;
         console.log("!!! Error sending SMS:", err.response ? err.response.data : err.message);
         return false;  // ✅ Return false if failed
